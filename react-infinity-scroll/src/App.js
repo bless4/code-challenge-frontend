08d582caedd7a4abb1e4ebbd4637ced92a28f43b
Martin Sone <kwangmart12@gmail.com>
2021-03-26 14:40:28 +0100

    header added

---
+++ react-infinity-scroll/src/App.js
@@ -0,0 +1,13 @@
+
+import Heading from './components/Heading/Heading'
+
+
+function App() {
+  return (
+    <div>
+      <Heading/>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-infinity-scroll/src/components/Heading/Heading.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Heading.css'
+
+class Heading extends Component {
+    render() {
+        return (
+            <div className="heading-text">
+               <h>Unplash Images</h> 
+            </div>
+        );
+    }
+}
+
+export default Heading;
